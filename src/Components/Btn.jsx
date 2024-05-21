bad6d419f13541f9632a1d3db8a9852e133f3899
Vaibhav Kumar <vaibhavkumar@Vaibhavs-MacBook-Air.local>
2024-05-21 15:37:02 +0530

    cleanup and mui setup

---
+++ src/Components/Btn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@mui/material'
+
+const Btn = () => {
+  return (
+    <>
+        <Button variant="contained">Hello world</Button>
+    </>
+  )
+}
+
+export default Btn
---
+++ src/Components/button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@mui/material'
+
+const button = () => {
+  return (
+    <>
+    <Button variant="contained">Hello world</Button>
+       
+    </>
+  )
+}
+
+export default button
